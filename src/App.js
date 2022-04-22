311827bb6920502f4e777a8dfa231e0d8c7c7643
Prizuri Hartadi <61555718+prizuri@users.noreply.github.com>
2022-04-22 11:29:18 +0700

    add feat: login, create playlist, search, and add playlist music item

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom"
+import UserAuth from './components/Auth/UserAuth';
+import CreatePlaylist from './components/CreatePlaylist/CreatePlaylist';
+import Search from './components/MusicData/Search';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/create-playlist">
+          <CreatePlaylist />
+          <Search />
+        </Route>
+        <Route path="/">
+          <UserAuth />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
